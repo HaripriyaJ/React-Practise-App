ee37b3262c14766897b4c0cbd4e81b9de0f61599
haripriya <haripriya@qburst.com>
2021-06-04 15:02:16 +0530

    Add page not found

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,17 @@ import {
 } from "react-router-dom";
 import Home from './common/components/Home';
 import PrivateRoute from './common/components/PrivateRoute';
+import PageNotFound from './common/components/PageNotFound';
 
 function App() {
+  // Note: Specify more specific routes first and general routes towards the end
   return (
       <Switch>
         <PrivateRoute path="/home">
-          <Home></Home>
+          <Home/>
         </PrivateRoute>
-        <Route path="/login" component={Login}/>
+        <Route path="/login" component={Login} exact={true}/>
+        <Route path="**" component={PageNotFound}/> {/* Wildcard route*/}
       </Switch>
   );
 }
---
+++ src/common/components/PageNotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Card.scss';
+
+function PageNotFound() {
+    return (
+        <div className="card">
+            <h3>Oops... Page not found!</h3>
+        </div>
+    );
+}
+export default PageNotFound;
