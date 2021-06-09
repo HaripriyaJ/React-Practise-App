39f5fdf9c12f8ab31d605fca4de43d4465420fa3
J_Haripriya <haripriya@qburst.com>
2021-06-09 17:43:39 +0530

    Bulma helper class for spacing

--- src/common/components/side-nav/SideNavBar.tsx
+++ src/common/components/side-nav/SideNavBar.tsx
@@ -7,23 +7,23 @@ import { Link } from 'react-router-dom';
 function SideNavBar(props: {options: ISideNavBarOptions[]}) {
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-brand">
+            <div className="side-navbar">
+                <div className="side-navbar-brand mr-1 mb-2 ml-1">
                     {/* Logo (or) brand Name */}
                     <img src={logo} className="app-logo" alt="logo" />
                     <h4>My React App</h4>
                 </div>
-                <div className="navbar-menu">
+                <div className="side-navbar-menu mr-5 ml-5">
                     {props.options.map(option => 
-                        <div key={option.name} id={option.name} className="navbar-item" title={option.title}>
-                            <span className="icon">
+                        <div key={option.name} id={option.name} className="side-navbar-item mt-5 mb-5" title={option.title}>
+                            <span className="nav-icon p-2">
                                 <i className={"fa fa-" + option.icon}></i>
                             </span>
-                            <Link to={option.path} className="icon-text">{option.displayText}</Link>
+                            <Link to={option.path} className="icon-text pl-2">{option.displayText}</Link>
                         </div>) 
                     }        
                 </div>
-            </nav>
+            </div>
         </>
     );
 }
